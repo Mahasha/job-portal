b0f20589d395f1801bd2e193f0758a527eac0625
Retshepile Mahasha <retshepilem@knowledgeobjects.com>
2024-06-08 23:41:15 +0200

    Remove redundant repository classes and clean up TokenRepository
    Removed unnecessary ApplicantRepository and ApplicationRepository classes. Also streamlined the TokenRepository class by removing the findAllValidTokenByUser method, focusing functionality on token retrieval.

--- src/main/java/co/za/thebrighthouse/jobportal/repository/ApplicantRepository.java
+++
@@ -1,7 +0,0 @@
-package co.za.thebrighthouse.jobportal.repository;
-
-import co.za.thebrighthouse.jobportal.entity.Applicant;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ApplicantRepository extends JpaRepository<Applicant, Long> {
-}
--- src/main/java/co/za/thebrighthouse/jobportal/repository/ApplicationRepository.java
+++
@@ -1,7 +0,0 @@
-package co.za.thebrighthouse.jobportal.repository;
-
-import co.za.thebrighthouse.jobportal.entity.Application;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ApplicationRepository extends JpaRepository<Application, Long> {
-}
